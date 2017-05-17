72bcf47efbe1fd3ea520b46da25c2612bdf10fa7
Richard Wilson <richard@freerunningtech.com>
2017-05-17 13:07:01 -0700

    wip

--- app/assets/javascripts/spree/backend/solidus_paypal_braintree.js
+++ app/assets/javascripts/spree/backend/solidus_paypal_braintree.js
@@ -1,43 +1,84 @@
-//= require solidus_paypal_braintree/constants
-//= require solidus_paypal_braintree/hosted_form
-
+//= require solidus_paypal_braintree/backend
+//
 $(function() {
-  var $paymentForm = $("#new_payment"),
-      $hostedFields = $("[data-braintree-hosted-fields]");
-
   function onError (err) {
     var msg = err.name + ": " + err.message;
     show_flash("error", msg);
     console.error(err);
   }
 
-  // exit early if we're not looking at the New Payment form, or if no
-  // SolidusPaypalBraintree payment methods have been configured.
-  if (!$paymentForm.length || !$hostedFields.length) { return; }
+  function braintreeError (err) {
+    onError(err);
+    enableSubmit();
+  }
 
-  $hostedFields.each(function() {
-    var $this = $(this),
-        $new = $("[name=card]", $this);
+  function enableSubmit() {
+    /* If we're using jquery-ujs on the frontend, it will automatically disable
+     * the submit button, but do so in a setTimeout here:
+     * https://github.com/rails/jquery-rails/blob/master/vendor/assets/javascripts/jquery_ujs.js#L517
+     * The only way we can re-enable it is by delaying longer than that timeout
+     * or stopping propagation so their submit handler doesn't run. */
+    if ($.rails) {
+      setTimeout(function () {
+        $.rails.enableFormElement($submitButton);
+        $submitButton.attr("disabled", false).removeClass("disabled").addClass("primary");
+      }, 100);
+    } else {
+      $submitButton.attr("disabled", false).removeClass("disabled").addClass("primary");
+    }
+  }
 
-    var id = $this.data("id");
+  function disableSubmit() {
+    $submitButton.attr("disabled", true).removeClass("primary").addClass("disabled");
+  }
 
-    var hostedFieldsInstance;
+  function addFormHook(braintreeForm, hostedField) {
+    $paymentForm.on("submit",function(event) {
+      var $field = $(hostedField);
 
-    $new.on("change", function() {
-      var isNew = $(this).val() === "new";
+      if ($field.is(":visible") && !$field.data("submitting")) {
+        var $nonce = $("#payment_method_nonce", $field);
 
-      function setHostedFieldsInstance(instance) {
-        hostedFieldsInstance = instance;
-        return instance;
-      }
+        if ($nonce.length > 0 && $nonce.val() === "") {
+          event.preventDefault();
+          disableSubmit();
 
-      if (isNew && hostedFieldsInstance == null) {
-        braintreeForm = new SolidusPaypalBraintree.createHostedForm(id);
-        braintreeForm.initialize().
-          then(setHostedFieldsInstance).
-          then(braintreeForm.addFormHook(onError)).
-          fail(onError)
+          braintreeForm.tokenize(function(error, payload) {
+            if (error) {
+              braintreeError(error);
+            } else {
+              $nonce.val(payload.nonce);
+              $paymentForm.submit();
+            }
+          });
+        }
       }
     });
-  });
+  }
+
+  var $paymentForm = $("#new_payment"),
+      $hostedFields = $("[data-braintree-hosted-fields]");
+  var $submitButton = $("input[type='submit']", $paymentForm);
+
+  // If we're not using hosted fields, the form doesn't need to wait.
+  if ($hostedFields.length > 0) {
+    disableSubmit();
+
+    var fieldPromises = $hostedFields.map(function(index, field) {
+      var $this = $(this);
+      var id = $this.data("id");
+
+      var braintreeForm = new SolidusPaypalBraintree.createHostedForm(id);
+
+      var formInitializationSuccess = function(formObject) {
+        addFormHook(formObject, field);
+      }
+
+      return braintreeForm.initialize().
+        then(braintreeForm.createHostedFields.bind(braintreeForm)).
+        then(formInitializationSuccess, braintreeError)
+    });
+
+    $.when.apply($, fieldPromises).done(enableSubmit);
+  }
 });
